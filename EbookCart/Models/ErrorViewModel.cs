a56de27b6d7be39fc06b401df988119eb7adf941
vijay.pk061@gmail.com <vijay.pk061@gmail.com>
2025-09-27 22:09:24 +0530

    Created MVC folder

---
+++ EbookCart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EbookCart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
